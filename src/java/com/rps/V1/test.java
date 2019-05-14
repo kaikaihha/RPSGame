a90dd98d136d2f117706f1a90686dd930b9a6093
kaikai <kaikaihha@outlook.com>
2019-05-14 08:25:44 +0800

    Initial commit

--- src/java/com/rps/V1/test.java
+++ src/java/com/rps/V1/test.java
@@ -1,9 +1,15 @@
 package com.rps.V1;
 
+import org.junit.Test;
+
 public class test {
 
 
     public static void main(String[] args) {
         new RPSGameV1();
     }
+//    @Test
+//    public void test(){
+//        new RPSGameV1();
+//    }
 }
